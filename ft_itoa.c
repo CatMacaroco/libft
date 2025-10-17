1d94634049e192b7c10eaa950e99f0fbab82feec
Catarina Macaroco <cmacaroc@f1r4s1.codam.nl>
2025-10-17 12:57:44 +0200

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,60 +6,60 @@
 /*   By: cmacaroc <cmacaroc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 10:36:11 by cmacaroc          #+#    #+#             */
-/*   Updated: 2025/10/16 17:05:27 by cmacaroc         ###   ########.fr       */
+/*   Updated: 2025/10/17 12:56:44 by cmacaroc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int num_length(int n)
+static int	num_length(int n)
 {
-    int length;
-    
-    length = 0;
-    if (n <= 0)
-    {
-        length++;
-        n = -n; //makes it positive
-    }
-    while (n > 0)
-    {
-        n = n / 10;
-        length++;
-    }
-    return (length);
+	int	length;
+
+	length = 0;
+	if (n <= 0)
+	{
+		length++;
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		length++;
+	}
+	return (length);
 }
 
-char *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-    int length;
-    char *str;
-    long nb;
+	char	*str;
+	int		length;
 
-    length = num_length(n);
-    nb = n;
-    str = (char *)malloc(sizeof(char) * (length + 1));
-    if (!str)
-    {
-        return (0);
-    }
-    str[length] = '\0';
-    while (nb < 0)
-    {
-        str[0] = '-';
-        nb = -nb;
-    }
-    while (--length >= 0 && str[length] != '-')
-    {
-        str[length] = (nb % 10) + '0';
-        nb = nb / 10;
-    }
-    return(str);
+	length = num_length(n);
+	str = malloc(sizeof(char) * (length + 1));
+	if (!str)
+		return (0);
+	str[length] = '\0';
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return (0);
+	}
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = -n;
+	}
+	while (n > 0)
+	{
+		str[--length] = (n % 10) + '0';
+		n = n / 10;
+	}
+	return (str);
 }
 
-int main(void)
-{
-    int n = 178;
-    printf("%s", ft_itoa(n));
-    return (0);
-}
+// int main(void)
+// {
+//     int n = 178;
+//     printf("%s", ft_itoa(n));
+//     return (0);
+// }
